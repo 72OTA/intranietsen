6dc4e00f8a93eda0b61538da49bcfbfb0f7798b9
72OTA <f.andradevalenzuela@gmail.com>
2017-11-16 14:20:37 -0400

    retoque de codigo.

--- app/templates/.cache/8b/8b389e0924f7f6d70ec133238727f7bbbef3008635f89755ee323adf263bf9a2.php
+++ app/templates/.cache/8b/8b389e0924f7f6d70ec133238727f7bbbef3008635f89755ee323adf263bf9a2.php
@@ -17,84 +17,64 @@ class __TwigTemplate_2bc28d4f5231d8cd70cd7f053a51bb7105d7ac980881404e89bcf671075
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 1
-        echo "
-                          <div id=\"modal_responder_solicitud\" class=\"modal fade\" role=\"dialog\">
-                            <div class=\"modal-dialog\">
-                              <div class=\"modal-content\">
-                                <div class=\"modal-header\">
-                                  <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>
-                                  <h4 class=\"modal-title\">Responder solicitud de horas extra</h4>
-                                </div>
-                                <div class=\"modal-body\">
-                                  <form name=\"form_respuesta\" id=\"form_respuesta\" action=\"\" method=\"POST\">
-                                      <center>
-                                        <div class=\"form-group\">
-                                            <label for=\"motivo\">Motivo de respuesta:</label>
-                                            <input type=\"text\" class=\"form-control\" name=\"motivo_respuesta\" id=\"motivo_respuesta\" >
-                                            <input type=\"hidden\" class=\"form-control\" value=\"";
-        // line 15
+        echo "<div id=\"modal_responder_solicitud\" class=\"modal fade\" role=\"dialog\">
+  <div class=\"modal-dialog\">
+    <div class=\"modal-content\">
+      <div class=\"modal-header\">
+        <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>
+        <h4 class=\"modal-title\">Responder solicitud de horas extra</h4>
+      </div>
+      <div class=\"modal-body\">
+        <form name=\"form_respuesta\" id=\"form_respuesta\" action=\"\" method=\"POST\">
+          <center>
+            <div class=\"form-group\">
+              <label for=\"motivo\">Motivo de respuesta:</label>
+              <input type=\"text\" class=\"form-control\" name=\"motivo_respuesta\" id=\"motivo_respuesta\">
+              <input type=\"hidden\" class=\"form-control\" value=\"";
+        // line 14
         echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), ($context["id"] ?? null), "id", array()), "html", null, true);
-        echo "\" name=\"id_respuesta\" id=\"id_respuesta\" >
-                                        </div>
-                                          <button class=\"btn btn-success\" type=\"button\" id=\"btn_aprobar\"><span>Aprobar</span></button>
-                                          <button class=\"btn btn-danger\" type=\"button\" id=\"btn_rechazar\"><span>Rechazar</span></button>
-                                      </center>
-                                      <br>
-                                      <!-- <table class=\"table table-bordered\">
-                                        <thead>
-                                          <tr>
-                                            <th>RUT</th>
-                                          </tr>
-                                        </thead>
-                                        <tbody>
-                                          ";
-        // line 28
+        echo "\" name=\"id_respuesta\" id=\"id_respuesta\">
+            </div>
+            <button class=\"btn btn-success\" type=\"button\" id=\"btn_aprobar\">
+              <span>Aprobar</span></button>
+            <button class=\"btn btn-danger\" type=\"button\" id=\"btn_rechazar\">
+              <span>Rechazar</span></button>
+          </center>
+          <br>
+          <!-- <table class=\"table table-bordered\"> <thead> <tr> <th>RUT</th> </tr> </thead> <tbody> ";
+        // line 22
         $context["No"] = 1;
-        // line 29
-        echo "                                          ";
+        echo " ";
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable(($context["horas_extras"] ?? null));
         foreach ($context['_seq'] as $context["_key"] => $context["d"]) {
-            // line 30
-            echo "                                            <tr>
-                                              <td>";
-            // line 31
+            echo " <tr> <td>";
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "rut", array()), "html", null, true);
-            echo "</td>
-                                              <td class='center'>
-                                                  <form class=\"\" action=\"\" name=\"form_id_mod\" id=\"form_id_mod\" method=\"post\">
-                                                      <input type=\"hidden\" id=\"id_hx_mod\" name=\"id_hx_mod\">
-                                                  </form>
-                                              </td>
-                                            </tr>
-                                          ";
-            // line 38
+            echo "</td> <td class='center'> <form class=\"\" action=\"\" name=\"form_id_mod\" id=\"form_id_mod\"
+          method=\"post\"> <input type=\"hidden\" id=\"id_hx_mod\" name=\"id_hx_mod\"> </form> </td> </tr> ";
+            // line 23
             $context["No"] = (($context["No"] ?? null) + 1);
-            // line 39
-            echo "                                          ";
+            echo " ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['d'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 40
-        echo "                                          </tr>
-                                        </tbody>
-                                      </table> -->
-                                  </form>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
+        echo " </tr> </tbody> </table> -->
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
 ";
-        // line 49
+        // line 30
         $this->displayBlock('appScript', $context, $blocks);
     }
 
     public function block_appScript($context, array $blocks = array())
     {
-        // line 50
-        echo "    <script src=\"views/app/js/horasextra/horasextra.js\"></script>
+        // line 31
+        echo "  <script src=\"views/app/js/horasextra/horasextra.js\"></script>
 ";
     }
 
@@ -110,7 +90,7 @@ class __TwigTemplate_2bc28d4f5231d8cd70cd7f053a51bb7105d7ac980881404e89bcf671075
 
     public function getDebugInfo()
     {
-        return array (  97 => 50,  91 => 49,  80 => 40,  74 => 39,  72 => 38,  62 => 31,  59 => 30,  54 => 29,  52 => 28,  36 => 15,  20 => 1,);
+        return array (  77 => 31,  71 => 30,  56 => 23,  46 => 22,  35 => 14,  20 => 1,);
     }
 
     public function getSourceContext()
--- app/templates/.cache/c1/c15e0fff343226bf60eb4ed72881b25a194d2992bfb95b055d399ed274042182.php
+++ app/templates/.cache/c1/c15e0fff343226bf60eb4ed72881b25a194d2992bfb95b055d399ed274042182.php
@@ -40,16 +40,17 @@ class __TwigTemplate_1e4c63510a863d616443c41b2e668d059253e1098c12ef71995b564dc1f
     {
         // line 7
         echo "  <section class=\"content-header\">
-      <h4>
-        <i class=\"fa fa-user\"></i> GESTION DE HORAS EXTRA
-        </a>
-      </h4>
-  </section>
-  <section class=\"content\">
-    <div class=\"row\">
-      <div class=\"col-md-12\">
-        <div class=\"box box-primary\">
-          <div class=\"box-body\">
+    <h4>
+      <i class=\"fa fa-user\"></i>
+      GESTION DE HORAS EXTRA
+    </a>
+  </h4>
+</section>
+<section class=\"content\">
+  <div class=\"row\">
+    <div class=\"col-md-12\">
+      <div class=\"box box-primary\">
+        <div class=\"box-body\">
           <table id=\"dataTables1\" class=\"table table-bordered\">
             <thead>
               <tr>
@@ -64,89 +65,91 @@ class __TwigTemplate_1e4c63510a863d616443c41b2e668d059253e1098c12ef71995b564dc1f
             </thead>
             <tbody>
               ";
-        // line 31
-        $context["No"] = 1;
         // line 32
+        $context["No"] = 1;
+        // line 33
         echo "              ";
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable(($context["horas_extras"] ?? null));
         foreach ($context['_seq'] as $context["_key"] => $context["d"]) {
             if ((false != ($context["horas_extras"] ?? null))) {
-                // line 33
-                echo "              ";
+                // line 34
+                echo "                ";
                 if ((twig_get_attribute($this->env, $this->getSourceContext(), ($context["owner_user"] ?? null), "id_user", array(), "array") == twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "id_user", array()))) {
-                    // line 34
-                    echo "                <tr>
-                  <td>";
                     // line 35
+                    echo "                  <tr>
+                    <td>";
+                    // line 36
                     echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "fecha_creacion", array()), "html", null, true);
                     echo "</td>
-                  <td>";
-                    // line 36
+                    <td>";
+                    // line 37
                     echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "fecha_solicitud", array()), "html", null, true);
                     echo "</td>
-                  <td>";
-                    // line 37
+                    <td>";
+                    // line 38
                     echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "hora_desde", array()), "html", null, true);
                     echo "</td>
-                  <td>";
-                    // line 38
+                    <td>";
+                    // line 39
                     echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "hora_hasta", array()), "html", null, true);
                     echo "</td>
-                  <td>";
-                    // line 39
+                    <td>";
+                    // line 40
                     echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "motivo_solicitud", array()), "html", null, true);
                     echo "</td>
-                  <td>";
-                    // line 40
+                    <td>";
+                    // line 41
                     echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "estado", array()), "html", null, true);
                     echo "</td>
-                  <td class='center' width='150'>
-                    ";
-                    // line 42
+                    <td class='center' width='150'>
+                      ";
+                    // line 43
                     if ((twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "estado", array()) == "Pendiente")) {
-                        // line 43
+                        // line 44
                         echo "                        <button type=\"button\" id=\"btn_revisar\" title='Revisar' class=\"btn btn-success btn-sm\" data-toggle=\"modal\" onclick=\"modal_responder_solicitud('";
                         echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->getSourceContext(), $context["d"], "id_enc_hx", array()), "html", null, true);
-                        echo "')\"><i class='glyphicon glyphicon-eye-open'></i></button>
-                        </a>
+                        echo "')\">
+                          <i class='glyphicon glyphicon-eye-open'></i>
+                        </button>
+                      </a>
                     ";
                     }
-                    // line 46
+                    // line 49
                     echo "                  </td>
                 </tr>
-                ";
+              ";
                 }
-                // line 49
-                echo "                ";
-                $context["No"] = (($context["No"] ?? null) + 1);
-                // line 50
+                // line 52
                 echo "              ";
+                $context["No"] = (($context["No"] ?? null) + 1);
+                // line 53
+                echo "            ";
             }
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['d'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 51
-        echo "              </tr>
-            </tbody>
+        // line 54
+        echo "          </tr>
+        </tbody>
 
-          </table>
-          </div>
-        </div>
-      </div>
+      </table>
     </div>
-  </section>
+  </div>
+</div>
+</div>
+</section>
 ";
-        // line 60
-        $this->loadTemplate("rrhh/horasextra/mostrar_hora_extra", "rrhh/horasextra/revisar_horas_extras_pendientes.twig", 60)->display($context);
+        // line 63
+        $this->loadTemplate("rrhh/horasextra/mostrar_hora_extra", "rrhh/horasextra/revisar_horas_extras_pendientes.twig", 63)->display($context);
     }
 
-    // line 62
+    // line 65
     public function block_appScript($context, array $blocks = array())
     {
-        // line 63
-        $this->loadTemplate("rrhh/horasextra/datatables_opciones", "rrhh/horasextra/revisar_horas_extras_pendientes.twig", 63)->display($context);
+        // line 66
+        $this->loadTemplate("rrhh/horasextra/datatables_opciones", "rrhh/horasextra/revisar_horas_extras_pendientes.twig", 66)->display($context);
     }
 
     public function getTemplateName()
@@ -161,7 +164,7 @@ class __TwigTemplate_1e4c63510a863d616443c41b2e668d059253e1098c12ef71995b564dc1f
 
     public function getDebugInfo()
     {
-        return array (  149 => 63,  146 => 62,  142 => 60,  131 => 51,  124 => 50,  121 => 49,  116 => 46,  109 => 43,  107 => 42,  102 => 40,  98 => 39,  94 => 38,  90 => 37,  86 => 36,  82 => 35,  79 => 34,  76 => 33,  70 => 32,  68 => 31,  42 => 7,  39 => 6,  33 => 3,  30 => 2,  11 => 1,);
+        return array (  152 => 66,  149 => 65,  145 => 63,  134 => 54,  127 => 53,  124 => 52,  119 => 49,  110 => 44,  108 => 43,  103 => 41,  99 => 40,  95 => 39,  91 => 38,  87 => 37,  83 => 36,  80 => 35,  77 => 34,  71 => 33,  69 => 32,  42 => 7,  39 => 6,  33 => 3,  30 => 2,  11 => 1,);
     }
 
     public function getSourceContext()
